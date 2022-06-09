94fc6769623c22cad40a882b91aea80d775da0d3
DESKTOP-JHOE41P\Mohadese <m.ahval303@gmail.com>
2022-06-09 15:26:40 +0430

    create ui, add product to cart and update cart, save storage

---
+++ scripts/shoppingCart/products.js
@@ -0,0 +1,25 @@
+export const productsData = [{
+        id: 1,
+        title: "bed room blue",
+        price: 10.99,
+        imageUrl: "../../images/12.jpg"
+    },
+    {
+        id: 2,
+        title: "bed room red",
+        price: 23.4,
+        imageUrl: "../../images/13.jpg"
+    },
+    {
+        id: 3,
+        title: "bed room black",
+        price: 59.1,
+        imageUrl: "../../images/14.jpg"
+    },
+    {
+        id: 4,
+        title: "bed room green",
+        price: 76.83,
+        imageUrl: "../../images/15.jpg"
+    },
+]
